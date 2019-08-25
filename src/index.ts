5c4bd02379c4bad0a092a61f6ea7575e833e2147
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-25 23:43:48 +0530

    Running an Analysis!

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { ReadFileSyncOptions } from "./ts-lib/types";
 
-const matches = fs
+const matches: string[][] = fs
   .readFileSync("football.csv", {
     encoding: "utf-8",
   } as ReadFileSyncOptions)
@@ -10,4 +10,13 @@ const matches = fs
     return row.split(",");
   });
 
-console.log(matches);
+let manUnitedWins = 0;
+for (const match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins += 1;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins += 1;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
